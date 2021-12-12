44df69d0e0b6a7db3b791967ce3eae1a0161b3b3
Grégory <gc.webmaster69@gmail.com>
2021-12-12 15:13:01 +0100

    add server file

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+// const { application } = require('express');
+require("dotenv").config();
+
+const app = express()
+
+var corOptions = {
+  origin: 'https://localhost:8081'
+}
+
+// midleware
+
+app.use(cors(corOptions))
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+// testing api
+
+app.get('/', (req, res) => {
+  res.json({message: 'hello test'})
+})
+
+// port
+
+const PORT = process.env.PORT || 3000
+
+// server
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
